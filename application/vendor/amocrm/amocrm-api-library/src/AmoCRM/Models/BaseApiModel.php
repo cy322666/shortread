0b4001c10823c34f60bd32ba395ccc605489f5aa
cy322666 <cy322666@gmail.com>
2026-03-17 13:24:41 +0200

    first comm

---
+++ application/vendor/amocrm/amocrm-api-library/src/AmoCRM/Models/BaseApiModel.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace AmoCRM\Models;
+
+use AmoCRM\Support\Str;
+
+use function is_callable;
+
+abstract class BaseApiModel
+{
+    /**
+     * @return array
+     */
+    abstract public function toArray(): array;
+
+    /**
+     * @return array
+     */
+    public static function getAvailableWith(): array
+    {
+        return [];
+    }
+
+    /**
+     * @param string|null $requestId
+     * @return array
+     */
+    abstract public function toApi(?string $requestId = null): array;
+
+    public function __get($name)
+    {
+        $methodName = 'get' . Str::camel(Str::ucfirst($name));
+        if (method_exists($this, $methodName)) {
+            return $this->$methodName();
+        } else {
+            return null;
+        }
+    }
+
+    public function __set($name, $value)
+    {
+        $methodName = 'set' . Str::camel(Str::ucfirst($name));
+        
+        if (method_exists($this, $methodName) && is_callable([$this, $methodName])) 
+        {
+            $method = new \ReflectionMethod($this, $methodName);
+            $params = $method->getParameters();
+            $param  = isset($params[0]) ? $params[0] : null;
+
+            $type = null;
+            if ($param !== null && method_exists($param, 'getType')) 
+            {
+                $type = $param->getType();
+            }
+            
+            if ($type instanceof \ReflectionNamedType && !$type->isBuiltin()) 
+            {
+                $class = $type->getName();
+
+                if (is_array($value) && method_exists($class, 'fromArray')) 
+                {
+                    $value = $class::fromArray($value);
+                }
+            }
+
+            $this->$methodName($value);
+        }
+    }
+}
